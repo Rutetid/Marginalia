45db88fe883de327eb1905ef35effdab6a362b72
Rutetid <50543186+Rutetid@users.noreply.github.com>
2024-05-17 23:46:05 +0530

    made navbar, working on homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
+import Homepage from "./Components/Homepage";
 
+import "./index.css";
+import Login from "./Components/Login";
 function App() {
   return (
-    <Navbar/>
-  )
+			<>
+				<Navbar />
+				<Homepage />
+        <Login />
+			</>
+		);
 }
 
 export default App;
