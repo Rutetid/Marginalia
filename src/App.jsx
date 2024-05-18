73a02a5ab1b3d071fec09728d29fc28ff7e098d3
Rutetid <50543186+Rutetid@users.noreply.github.com>
2024-05-18 17:18:01 +0530

    implemented new homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import Navbar from "./Components/Navbar";
 import Homepage from "./Components/Homepage";
+import Home from "./Components/Home";
 
 import "./index.css";
 import Login from "./Components/Login";
 function App() {
   return (
 			<>
+				
 				<Navbar />
-				<Homepage />
+				<Home />
+				
+				
         <Login />
 			</>
 		);
